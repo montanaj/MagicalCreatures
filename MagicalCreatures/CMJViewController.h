4eba8d4a5ac989150d16bdbb72e4708c6a581f7e
montanaj <clairejencks@gmail.com>
2014-03-18 16:29:19 -0500

    initial commit

---
+++ MagicalCreatures/CMJViewController.h
@@ -0,0 +1,13 @@
+//
+//  CMJViewController.h
+//  MagicalCreatures
+//
+//  Created by Claire Jencks on 3/18/14.
+//  Copyright (c) 2014 Claire Jencks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CMJViewController : UIViewController
+
+@end
